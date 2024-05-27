deb74a474761dd1c05e47a34dc3a573179d65feb
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2024-05-27 20:53:59 +0900

    2024/05/27 practic sakaeから

--- practic/resources/js/app.js
+++ practic/resources/js/app.js
@@ -1,10 +1,1 @@
 import './bootstrap';
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(cors({
-    origin: 'http://localhost:8573', //アクセス許可するオリジン
-    credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
-    optionsSuccessStatus: 200 //レスポンスstatusを200に設定
-}))
