df7537e2331e61ce2245213b219455ff6bb0582c
Yasuaki_Shintomi <shintomi.sh@gmail.com>
2024-09-15 08:36:48 +0900

    2024/09/15 SVから

--- practic/routes/console.php
+++ practic/routes/console.php
@@ -34,9 +34,9 @@ Artisan::command('inspire', function () {
         Schedule::command('backup:run')
                  ->weeklyOn(0, '06:10');                // 毎週日曜日(0)AM6:00に実行する
 
-        Schedule::command('cache:clear')
-                ->dailyAt('23:00')                      // 毎日AM10:00に実行する
-                ->onSuccess(function () {
-                    Log::info('schedule cache:clear 成功');
-        });
+        // Schedule::command('cache:clear')
+        //         ->dailyAt('23:00')                      // 毎日AM23:00に実行する
+        //         ->onSuccess(function () {
+        //             Log::info('schedule cache:clear 成功');
+        // });
 
